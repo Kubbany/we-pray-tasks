fd8cfa448b39fbe60504add7eab7f438aca453f6
Kubbany <2021170289@cis.asu.edu.eg>
2025-08-31 16:38:12 +0300

    add fonts

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 class AppStyles {
   static TextStyle semiBold19 = const TextStyle(
     color: Colors.white,
+    fontFamily: 'Hanken Grotesk',
     fontSize: 19,
     fontWeight: FontWeight.w600,
   );
   static TextStyle medium19 = const TextStyle(
     fontSize: 19,
+    fontFamily: 'Hanken Grotesk',
     fontWeight: FontWeight.w500,
   );
   static TextStyle medium14 = const TextStyle(
+    fontFamily: 'DM Sans',
     fontSize: 14,
     fontWeight: FontWeight.w500,
   );
